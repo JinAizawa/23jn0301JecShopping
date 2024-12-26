7a8df519415f5cca19b51d7662d60ea2709ac643
JinAizawa <23jn0301@jec.ac.jp>
2024-12-27 02:15:04 +0900

    hyhh

--- helpers/config.php
+++ helpers/config.php
@@ -1,6 +1,5 @@
 <?php
 // DB接続設定
-define('DSN', 'sqlsrv:server=tcp:23jn0301db.database.windows.net,1433');
-define('DB_NAME', '23jn0301DB');
+define('DSN', 'sqlsrv:server=tcp:23jn0301db.database.windows.net,1433;Database=23jn0301DB');
 define('DB_USER', 'jndb');
 define('DB_PASSWORD', 'Pa$$word1234');
