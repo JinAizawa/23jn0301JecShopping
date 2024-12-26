acd177be4640e4e62b84d6e0f3bb23f877096e4f
JinAizawa <23jn0301@jec.ac.jp>
2024-12-27 02:51:47 +0900

    円マーク追加

--- goods.php
+++ goods.php
@@ -1,65 +1,69 @@
 <?php
-    require_once './helpers/GoodsDAO.php';
+require_once './helpers/GoodsDAO.php';
 
-    //URLリクエストパラメータに商品コードがセットされているとき
-    if(isset($_GET['goodscode'])){
-        //リクエストパラメータの商品コードを取得する
-        $goodscode=$_GET['goodscode'];
+//URLリクエストパラメータに商品コードがセットされているとき
+if (isset($_GET['goodscode'])) {
+    //リクエストパラメータの商品コードを取得する
+    $goodscode = $_GET['goodscode'];
 
-        //DBから商品データを取得する
-        $goodsDAO=new GoodsDAO();
-        $goods=$goodsDAO->get_goods_by_goodscode($goodscode);
-    }
-    ?>
+    //DBから商品データを取得する
+    $goodsDAO = new GoodsDAO();
+    $goods = $goodsDAO->get_goods_by_goodscode($goodscode);
+}
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
+
 <body>
     <meta charset="utf-8">
     <title>商品詳細</title>
-</head>    
+    </head>
 </body>
-    <?php include "header.php" ?>
+<?php include "header.php" ?>
 
-    <table>
-        <tr>
-            <td rowspan="5">
-                <img src="images/goods/<?=$goods->goodsimage ?>">
-            </td>
-            <td>
-                <?=$goods->goodsname?>
-            </td>    
-        </tr>
-        <tr>
-            <td>
-                <?=$goods->detail?>
-            </td>    
-        </tr>
-        <tr>
-            <td>
-                <?=number_format($goods->price)?>
-            </td>
-        </tr>
-        <tr>
-             <td>
-            <?=$goods->recommend?"おすすめ商品" :""?>
-            </td>  
-        </tr>
-        <tr>
-            <td>
-                <form action="cart.php" method="POST">
-                    個数
-                    <select name="num">
-                        <?php for($i=1; $i<=10; $i++): ?>
-                            <option value="<?= $i ?>"><?= $i ?></option>
-                        <?php endfor; ?>
+<table>
+    <tr>
+        <td rowspan="5">
+            <img src="images/goods/<?= $goods->goodsimage ?>">
+        </td>
+        <td>
+            <?= $goods->goodsname ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?= $goods->detail ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?= '￥' . number_format($goods->price) ?>
 
-                    </select>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?= $goods->recommend ? "おすすめ商品" : "" ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <form action="cart.php" method="POST">
+                個数
+                <select name="num">
+                    <?php for ($i = 1; $i <= 10; $i++): ?>
+                        <option value="<?= $i ?>"><?= $i ?></option>
+                    <?php endfor; ?>
 
-                    <input type="hidden" name="goodscode" value="<?=$goods->goodscode?>">
-                    <input type="submit" name="add" value="カートに入れる">
-                </form>
-            </td> 
-    </table>
+                </select>
+
+                <input type="hidden" name="goodscode" value="<?= $goods->goodscode ?>">
+                <input type="submit" name="add" value="カートに入れる">
+            </form>
+        </td>
+</table>
 </body>
+
 </html>
